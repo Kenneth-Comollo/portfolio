489d6f58dcc39aa8479d9c32680434cc8031657a
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-10-29 19:31:57 -0400

    Add files via upload

---
+++ contact-manager-git/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Edit <code>src/App.js</code> and save to reload.
+        </h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
