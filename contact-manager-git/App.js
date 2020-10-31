c9dcec2dfc30c2dc3e20bc2e5e5c5a817c4d8311
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-10-30 21:43:28 -0400

    Update App.js

--- contact-manager-git/App.js
+++ contact-manager-git/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Edit <code>src/App.js</code> and save to reload.
+          Contact Manager App
         </h1>
 
       </header>
