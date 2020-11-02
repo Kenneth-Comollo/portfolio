227ff85e666b7117840071087fc980d434fa6ff9
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-11-02 16:26:44 -0500

    Add files via upload

--- contact-manager-git/App.js
+++ contact-manager-git/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import React, { Component } from 'react';
+import AddPersonForm from "./Components/AddPersonForm";
+import PeopleList from "./Components/PeopleList";
+
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"]
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>
-          Contact Manager App
+          Contact Manager
         </h1>
-
       </header>
+      <AddPersonForm />
+      <PeopleList data={contacts} />
     </div>
   );
 }
