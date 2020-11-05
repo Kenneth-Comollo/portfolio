37ea1fd659925e3d1886bd8e7754d57ee73d353c
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-11-05 14:02:42 -0500

    Add files via upload

--- portfolio-page-github/Header.js
+++ portfolio-page-github/Header.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div className="row">
-        <h1>KC</h1>
-        <h1>Kenneth Comollo</h1>
+        <div className="col-md-4">
+          <h1>KC</h1>
+        </div>
+        <div className="col-md-4">
+          <h1>Kenneth Comollo</h1>
+        </div>
       </div>
     </div>
   );
