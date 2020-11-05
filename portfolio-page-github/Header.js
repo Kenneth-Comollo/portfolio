fbefbd289debfd2a4d7b8984d368d2729abef70f
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-11-04 22:23:48 -0500

    Add files via upload

--- portfolio-page-github/Header.js
+++ portfolio-page-github/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <h1>Logo</h1>
+        <h1>KC</h1>
         <h1>Kenneth Comollo</h1>
       </div>
     </div>
