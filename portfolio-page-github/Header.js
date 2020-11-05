a7be876bb1ee5855d256ea4421c95b1ef1602adf
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-11-04 22:22:52 -0500

    Add files via upload

---
+++ portfolio-page-github/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <h1>Logo</h1>
+        <h1>Kenneth Comollo</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
