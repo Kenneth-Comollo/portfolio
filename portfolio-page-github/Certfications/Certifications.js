9d31b2f8a83c4ee2c6a8af20b8c793c8382fba2a
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-11-06 14:18:46 -0500

    Add files via upload

--- portfolio-page-github/Certfications/Certifications.js
+++ portfolio-page-github/Certfications/Certifications.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
+import Sololearn from './Sololearn.js'
 
 class Certifications extends Component {
 
   state = {
-    certs: [
-      {name: 'C#', img: "img", org: "Sololearn", date: "September, 2020"},
-      {name: "jQuery", img: "img", org: "Sololearn", date: "July, 2020"},
-      {name: "C", img: "img", org: "Sololearn", date: "July, 2020"},
-      {name: "C++", img: "img", org: "Sololearn", date: "May, 2020"},
-      {name: "PHP", img: "img", org: "Sololearn", date: "March, 2020"},
-      {name: "Ruby", img: "img", org: "Sololearn", date: "February, 2020"},
-      {name: "JavaScript", img: "img", org: "Sololearn", date: "January, 2020"},
-      {name: "CSS", img: "img", org: "Sololearn", date: "April, 2019"},
-      {name: "HTML", img: "img", org: "Sololearn", date: "March, 2019"},
-      {name: "Java", img: "img", org: "Sololearn", date: "December, 2018"},
-      {name: "SQL", img: "img", org: "Sololearn", date: "August, 2018"},
-      {name: "Python 3", img: "img", org: "Sololearn", date: "May, 2018"},
-      {name: "C#", img: "img", org: "Sololearn", date: "December, 2017"}
+    solocerts: [
+      {name: 'C#', img: "img", org: "Sololearn", date: "September, 2020", id: 1},
+      {name: "jQuery", img: "img", org: "Sololearn", date: "July, 2020", id: 2},
+      {name: "C", img: "img", org: "Sololearn", date: "July, 2020", id: 3},
+      {name: "C++", img: "img", org: "Sololearn", date: "May, 2020", id: 4},
+      {name: "PHP", img: "img", org: "Sololearn", date: "March, 2020", id:5},
+      {name: "Ruby", img: "img", org: "Sololearn", date: "February, 2020", id: 6},
+      {name: "JavaScript", img: "img", org: "Sololearn", date: "January, 2020", id: 7},
+      {name: "CSS", img: "img", org: "Sololearn", date: "April, 2019", id: 8},
+      {name: "HTML", img: "img", org: "Sololearn", date: "March, 2019", id: 9},
+      {name: "Java", img: "img", org: "Sololearn", date: "December, 2018", id: 10},
+      {name: "SQL", img: "img", org: "Sololearn", date: "August, 2018", id: 11},
+      {name: "Python 3", img: "img", org: "Sololearn", date: "May, 2018", id: 12},
+      {name: "C#", img: "img", org: "Sololearn", date: "December, 2017", id: 13}
     ]
   }
 
@@ -24,6 +25,7 @@ class Certifications extends Component {
     return (
       <div className="certifications">
         <div>Certifications</div>
+        <Sololearn solocerts={this.state.solocerts}/>
       </div>
     );
   }
