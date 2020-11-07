4b85f24cb84c29da4887998cc26d49cfee7ec705
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-11-06 19:48:24 -0500

    Add files via upload

--- contact-manager-git/App.js
+++ contact-manager-git/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ContactManager from "./Components/ContactManager"
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+const initialState = {
+  contacts: ["James Smith", "Thomas Anderson", "Bruce Wayne"]
+};
 
+// Reducer Function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case "ADD_PERSON":
+      return {...state,
+      contacts: [...state.contacts, action.data]}
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Contact Manager
-        </h1>
+        <h1>Contact Manager</h1>
       </header>
-      <ContactManager data={contacts} />
+      <Provider store={store}>
+        <ContactManager />
+      </Provider>
     </div>
   );
 }
