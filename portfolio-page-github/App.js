a3e261149a69b85696dc1d6c4c122cb5b22066cd
Kenneth Comollo <45769147+snail-down@users.noreply.github.com>
2020-11-24 20:56:14 -0500

    Add files via upload

--- portfolio-page-github/App.js
+++ portfolio-page-github/App.js
@@ -1,9 +1,16 @@
-import Header from './Header.js'
+import React from "react";
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="navigation">
+        <div className="navigation-sub">
+          <a href="xxx" className="nav-item">Resume</a>
+          <a href="xxx" className="nav-item">Projects</a>
+          <a href="xxx" className="nav-item">About</a>
+        </div>
+      </div>
     </div>
   );
 }
